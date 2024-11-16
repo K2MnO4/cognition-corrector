c67604b621733d86f22fb9483b03453e0c8e6f19
ZHANG LIN HUI <93325625+15989488337@users.noreply.github.com>
2024-11-16 21:52:20 +0800

    Update ctrl_marker.py

--- Evaluate/Marker/ctrl_marker.py
+++ Evaluate/Marker/ctrl_marker.py
@@ -1,5 +1,5 @@
 from CTRLEval.ctrleval import CTRLEval
-
+import torch
 import jsonlines
 import numpy as np
 from tqdm import tqdm
@@ -7,11 +7,11 @@ from tqdm import tqdm
 # This function is only for evaluating the score in consistency
 def calc_ctrl_score(file_path):
     task = "topic"
-    device = "cpu"
-    # device = "cuda" if torch.cuda.is_available() else "cpu"
-    scorer = CTRLEval(iwf_dir='Evaluate/CTRLEval/data/iwf_full.txt',
-                        prompt_dir='Evaluate/CTRLEval/data/prompt_{}.txt'.format(task),
-                        verbal_dir='Evaluate/CTRLEval/data/verbal_{}.txt'.format(task),
+    # device = "cpu"
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    scorer = CTRLEval(iwf_dir='/home/ubuntu-user/Desktop/cognition-corrector-dev/Evaluate/CTRLEval/data/iwf_full.txt',
+                        prompt_dir='/home/ubuntu-user/Desktop/cognition-corrector-dev/Evaluate/CTRLEval/data/prompt_{}.txt'.format(task),
+                        verbal_dir='/home/ubuntu-user/Desktop/cognition-corrector-dev/Evaluate/CTRLEval/data/verbal_{}.txt'.format(task),
                         device=device)
     prefix_list = []
     data_list = []
@@ -22,6 +22,10 @@ def calc_ctrl_score(file_path):
             answer = line["answer"]
             generated_answer = line["generated_answer"]
 
+            if not generated_answer.strip():
+                print(f"Skipping index {i} due to empty generated_answer.")
+                continue
+            
             answer = answer.replace("\n", " ")
             answer = answer.replace("\t", " ")
             generated_answer = generated_answer.replace("\n", " ")
