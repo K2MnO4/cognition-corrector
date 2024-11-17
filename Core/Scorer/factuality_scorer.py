9d58b2f32b38f49406a630397ffe66f92311775b
K2MnO4 <1048117272@qq.com>
2024-11-17 16:26:52 +0800

    Temp: scorer

---
+++ Core/Scorer/factuality_scorer.py
@@ -0,0 +1,27 @@
+from opt_score import OPTScorer
+
+
+
+def factuality_score(gptscore_model_name, context, question, generated_answer, demo_num = 0):
+    PREFIX = {
+        0: f'''Based on the Context and Question, please generate the factual response. To do this, please consider these factors: Verifiability, Objectivity, and Reliability of Source. Note that this evaluation should be based on the best available medical knowledge.
+    Context: {context}
+    Question: {question}
+    Response: ''', }
+    prefix = PREFIX[demo_num]
+
+    srcs = [prefix]
+    tgts = [generated_answer]
+
+    check_point = "facebook/opt-350m" # default gpt-score model
+    if gptscore_model_name == "opt-125m":
+        check_point = "facebook/opt-125m"
+    elif gptscore_model_name == "opt-1.3b":
+        check_point = "facebook/opt-1.3b"
+    elif gptscore_model_name == "gpt2-medium":
+        check_point = "openai-community/gpt2-medium"
+    elif gptscore_model_name == "gpt2-large":
+          check_point = "openai-community/gpt2-large"
+
+    score_list = OPTScorer(device='cuda', max_length=1024, checkpoint=check_point).score(srcs, tgts, prompt_text="", batch_size=1)
+    gptscore = score_list[0]
---
+++ Core/corrector.py
@@ -0,0 +1,72 @@
+from transformers import pipeline
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from tqdm import tqdm
+
+import torch
+import argparse
+
+import jsonlines
+
+class Prompter:
+    def __init__(self, default_prompt=''):
+        self.default_prompt = default_prompt
+
+    def generate_prompt(self, instruction, question):
+        return f"{instruction} {question}"
+
+def encode_prompt(tokenizer, prompt):
+    return tokenizer.encode_plus(prompt, return_tensors='pt', padding='max_length', truncation=True, max_length=128)
+
+def generate_response(model, tokenizer, prompt, max_new_tokens = 256, top_k = 50, top_p = 0.95):
+    inputs = encode_prompt(tokenizer, prompt)
+    input_ids = inputs['input_ids'].to(device)
+    attention_mask = inputs['attention_mask'].to(device)
+
+    output = model.generate(
+        input_ids,
+        attention_mask=attention_mask,
+        # max_length=max_length,
+        max_new_tokens = max_new_tokens,
+        num_return_sequences=1,
+        do_sample=True,
+        top_k=top_k,
+        top_p=top_p,
+        pad_token_id=tokenizer.eos_token_id,
+        eos_token_id=tokenizer.eos_token_id,
+    )
+    generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
+    prompt_length = len(tokenizer.decode(input_ids[0], skip_special_tokens=True))
+    return  generated_text[prompt_length + 1:]
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input-file", type=str)
+    parser.add_argument("--threshold-fact", type=float, default=-1)
+    parser.add_argument("--threshold-consistency", type=float, default=-5)
+    parser.add_argument("--threshold-entailment", type=float, default=0.8)
+    parser.add_argument("--max-loop", type=int, default=1)
+
+
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    model_name = "Locutusque/gpt2-large-medical"
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForCausalLM.from_pretrained(model_name).to(device)
+
+    with jsonlines.open("Data Process/Datasets/pubmedqa/pub_data_copy.jsonl") as reader:
+        reader = list(reader)
+        for i, line in tqdm(enumerate(reader), total=len(reader)):
+            question = line['question']
+            prompter = Prompter('')
+            instruction = "Answer the following question."
+            prompt = prompter.generate_prompt(instruction, question)
+            response = generate_response(model, tokenizer, prompt)
+
+            line.update({'generated_answer': response})
+            writer = jsonlines.open("Data Process/Datasets/pubmedqa/output_data.jsonl", mode='a')
+            writer.write(line)
+            writer.close()
+
+
+    
+  
